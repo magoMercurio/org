5c1e55df562732aca6be76237031ace489160271
mago_d_Mercurio <davila.feria@gmail.com>
2023-04-07 11:02:34 -0500

    add Formulario component

--- src/App.js
+++ src/App.js
@@ -1,26 +1,13 @@
-import logo from './logo.svg';
 import './App.css';
 import Header from './componentes/Header/Header';
 
 
 function App() {
   return (
+    
     <div className="App">
-      <header className="App-header">
       <Header />
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Hola que tal !!!
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Aprende React
-        </a>
-      </header>
+      
     </div>
   );
 }
--- src/componentes/Header/Header.js
+++ src/componentes/Header/Header.js
@@ -1,6 +1,9 @@
+import './Header.css'
 
 function Header() {
-  return <img src='/img/header.png' alt='Header' />
+  return <header className='header'>
+    <img src='/img/header.png' alt='Org' />
+  </header>
     
 }
 
